8e1dce5bf8ebce3e7715acd1172e24d580e8dbbe
maek <jak.malkowski@gmail.com>
2025-03-20 20:37:54 +0100

    Initialized backend project with necessary dependencies

---
+++ applicationBackend/src/main/java/com/projektowanie/zespolowe/applicationbackend/ApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projektowanie.zespolowe.applicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBackendApplication.class, args);
+    }
+
+}
