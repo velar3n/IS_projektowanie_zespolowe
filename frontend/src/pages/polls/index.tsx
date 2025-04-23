e049f5038649a8277c68c1eb8e21c42edf396b3d
Kamil Kuziora <kam.kuziora@gmail.com>
2025-04-23 21:46:56 +0200

    feat: added polls table

--- frontend/src/pages/polls/index.tsx
+++ frontend/src/pages/polls/index.tsx
@@ -1,13 +1,20 @@
 import DataTable from '@/components/table';
 import { Stack } from '@chakra-ui/react';
 import exampleData from './data';
-import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import {
+  CellContext,
+  ColumnDef,
+  createColumnHelper,
+} from '@tanstack/react-table';
 import { PollRow } from './types';
 import BooleanCell from '@/components/table/components/BooleanCell';
 import { camelCaseToTitleCase } from '@/utils/text';
+import ActionCell from '@/components/table/components/ActionCell';
+import { useState } from 'react';
 
 const Polls = () => {
   const columnHelper = createColumnHelper<PollRow>();
+  const [data, setData] = useState(exampleData);
 
   const columns = [
     columnHelper.accessor('id', { cell: (ctx) => ctx.getValue() }),
@@ -28,12 +35,34 @@ const Polls = () => {
       cell: (ctx) => <BooleanCell value={ctx.getValue()} />,
       header: (ctx) => camelCaseToTitleCase(ctx.column.id),
     }),
+    {
+      id: 'action',
+      cell: (ctx: CellContext<PollRow, undefined>) => {
+        const currentRowId = ctx.row.index;
+        return (
+          <ActionCell
+            config={[
+              {
+                label: 'Delete',
+                value: 'delete',
+                destructive: true,
+                onClick: () =>
+                  setData((prev) =>
+                    prev.filter((_, index) => index !== currentRowId),
+                  ),
+              },
+            ]}
+          />
+        );
+      },
+      header: 'Actions',
+    },
   ];
 
   return (
     <Stack w="100%" h="100%" px="32px">
       <DataTable
-        data={exampleData}
+        data={data}
         columns={columns as ColumnDef<PollRow>[]}
         searchColumnId="name"
       />
